0b0f20c037930d724c5fe906da8d25da140cfe15
DavegTheMighty <dave@example.com>
2020-04-10 13:06:22 +0100

    Less/Webpack - use HtmlWebpackExternalsPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,38 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin');
 
 module.exports = {
     mode: 'development',
     entry: './src/js/app.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public/js'),
+        path: path.resolve(__dirname, 'public/'),
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: "My Form",
+          template: './src/original.html'
+        }),
+        new HtmlWebpackExternalsPlugin({
+            externals: [
+              {
+                module: 'google-share',
+                entry: {
+                  path: 'http://fonts.googleapis.com/css?family=Share:400,700',
+                  type: 'css',
+                },
+              },
+              {
+                module: 'google-open-sans',
+                entry: {
+                  path: 'http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700s',
+                  type: 'css',
+                },
+              },
+            ],
+        }),
+    ],
     module: {
         rules: [
             {
